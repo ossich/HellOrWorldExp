4fb7b115ca7ec439c3faa6e19f6a3cad6cb8599f
ossich <andrei.goldman@gmail.com>
2020-02-06 12:25:24 +0000

    add menue step 01

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+
 const path = require('path')
 const app = express()
 
+const homeRoutes = require('./routs/home')
 const pingRoutes = require('./routs/ping')
 
-// app.use('/ping',pingRoutes)
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'    
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views','views')
+
+app.use('/',homeRoutes)
+app.use('/ping',pingRoutes)
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'views','index.html'))
