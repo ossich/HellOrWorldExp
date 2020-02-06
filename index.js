f730631aaa65bac8f7861e43ff76359228d86f8b
ossich <andrei.goldman@gmail.com>
2020-02-06 10:24:15 +0000

    HellOrWorld - ex

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const path = require('path')
 const app = express()
-const PORT = process.env.PORT || 3000
 
-app.listen(PORT, ()=>{
-    console.log('HellOrWorld ?')
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'views','index.html'))
+})
+
+app.listen(3000,()=>{
+    console.log('Server is running.')
 })
