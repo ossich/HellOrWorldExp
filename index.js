40d9304fe390b7fe710d25539d7e14cb3795768e
ossich <andrei.goldman@gmail.com>
2020-02-09 10:53:21 +0000

    routs 000

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 
+const homeRoutes = require('./routs/home')
+const pingRoutes = require('./routs/ping')
+
 const path = require('path')
 const app = express()
 
-const homeRoutes = require('./routs/home')
-const pingRoutes = require('./routs/ping')
 
 const hbs = exphbs.create({
     defaultLayout: 'main',
@@ -16,16 +17,21 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views','views')
 
-app.use('/',homeRoutes)
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+
+app.use('/routs',homeRoutes)
 app.use('/ping',pingRoutes)
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views','index.html'))
-})
 
-app.get('/ping',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views','ping.html'))
-})
+
+// app.get('/',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'views','index.hbs'))
+// })
+
+// app.get('/ping',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'views','ping.html'))
+// })
 
 app.listen(3000,()=>{
     console.log('Server is running.')
